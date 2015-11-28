17f77efc78011ec8a070ea7d2cf433b790f4b8c3
Shunsuke Aoki <s-aoki@r-learning.co.jp>
2015-11-28 21:55:03 +0900

    refactoring

--- 07/VMtranslator/code_writer.rb
+++ 07/VMtranslator/code_writer.rb
@@ -9,7 +9,6 @@ class CodeWriter
   def initialize(file)
     @writer = File.open(file, "w")
     @output_files = [file]
-    @stack = 256
   end
 
   def set_file_name(file)
@@ -19,31 +18,24 @@ class CodeWriter
   def write_arithmetic(command)
     asms = []
 
-    if command == "add"
-      # アドレスをさかのぼる
-      asms << "@SP\n"
-      asms << "M=M-1\n"
-
-      # その時点で指しているアドレスの値をロードする
-      asms << "@SP\n"
-      asms << "A=M\n"
-      asms << "D=M\n"
-
-      asms << "@SP\n"
-      asms << "M=M-1\n"
-      asms << "@SP\n"
-      asms << "A=M\n"
-      asms << "D=D+M\n"
-      asms << "M=D\n"
+    case command
+    when "add"
+      asms << dec_sp
+      asms << load_sp # Mにロード
+      asms << dec_sp
+      asms << "@SP"
+      asms << "A=M"
+      asms << "D=D+M"
+      asms << "M=D"
 
       # ＠SPには計算結果が格納済みなので先頭アドレスを変えておく
-      asms << "@SP\n"
-      asms << "M=M+1\n"
+      asms << inc_sp
+    when "eq"
+    else
+      asms << ""
     end
 
-    asms.each do |asm|
-      @writer.write(asm)
-    end
+    write_asm(asms)
   end
 
   def write_push_pop(command, segment, index)
@@ -52,21 +44,37 @@ class CodeWriter
     # push: @SPが指すアドレスに値を格納して、SPの指すアドレスをインクリメントする
     if command == Parser::COMMANDS[:push]
       if "constant" == segment
-        asms << "@#{index}\n"
-        asms << "D=A\n"
+        asms << "@#{index}"
+        asms << "D=A"
 
-        asms << "@SP\n"
-        asms << "A=M\n" # @SP内にあるアドレスを取得
-        asms << "M=D\n" # 取得したアドレスに値をロード
-
-        # スタックポインタのアドレスをインクリメント
-        asms << "@SP\n"
-        asms << "M=M+1\n"
+        asms << load_sp
+        asms << inc_sp
       end
     end
 
-    asms.each do |asm|
-      @writer.write(asm)
+    write_asm(asms)
+  end
+
+  # スタックポインタのアドレスをインクリメント
+  def inc_sp
+    %w(@SP M=M+1)
+  end
+
+  def dec_sp
+    %w(@SP M=M-1)
+  end
+
+  def load_sp
+    [
+      "@SP",
+      "A=M",  # @SP内にあるアドレスを取得
+      "M=D"   # 取得したアドレスに値をロード
+    ]
+  end
+
+  def write_asm(asms)
+    asms.flatten.each do |asm|
+      @writer.write("#{asm}\n")
     end
   end
 
